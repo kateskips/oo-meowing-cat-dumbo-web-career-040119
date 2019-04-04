327e174b75c9c138de367f28b2d99f61976d4d13
Kate F <follett.katie@gmail.com>
2019-04-04 01:13:48 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,28 @@
-## code your solution here. 
+require 'pry'
+
+class Cat
+ attr_accessor :name
+ 
+
+ def name= (cat_name)
+   @name = cat_name
+ end
+ 
+ def name
+   @name
+ end
+ 
+ def meow= (cat_sound)
+   @meow = cat_sound
+ end
+ 
+ def meow
+   puts "meow!"
+ end
+end
+
+maru = Cat.new
+maru.name = "Maru"
+
+
+
